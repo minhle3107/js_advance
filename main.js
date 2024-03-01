87dba472597440d3e96d5c3bf9da46f6186c4601
minhle_3107 <lexuanminh3107@gmail.com>
2024-03-01 19:31:44 +0700

    update

--- main.js
+++ main.js
@@ -1,51 +1,54 @@
+const url = "http://localhost:3000/products";
+
+const fetchProducts = async () => {
+  const response = await fetch(url);
+  return response.json();
+};
+
+// (async () => {
+//   const data = await fetchProducts();
+//   console.log(data);
+// })();
+
+const deleteProduct = async (id) => {
+  await fetch(`${url}/${id}`, { method: "DELETE" });
+  alert("Bạn đã xóa thành công");
+  await render();
+};
+
 const render = async () => {
   const showProduct = document.querySelector(".show__product");
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-  const logoutBtn = document.getElementById("logoutBtn");
 
   if (!currentUser) {
-    logoutBtn.style.display = "none";
     window.location.href = "./login.html";
-    return;
   }
 
-  logoutBtn.style.display = "block";
-
-  const response = await fetch(`http://localhost:3000/products`);
-  const data = await response.json();
-
+  const data = await fetchProducts();
+  // console.log(data);
   showProduct.innerHTML = data
     .map(
-      ({ name, price, id }) => `
-      <tr>
-        <td>${name}</td>
-        <td>${price}</td>
+      (pro) => `<tr>
+        <td>${pro.id}</td>
+        <td>${pro.name}</td>
+        <td>${pro.price}</td>
         <td>
-          <a href="./edit.html?id=${id}"><button class="btn btn-warning">Update</button></a>
-          <button data-id="${id}" class="btn btn-danger btn__delete">Delete</button>
+          <a href="./edit.html?id=${pro.id}"><button class="btn btn-warning">Update</button></a>
+          <button data-id="${pro.id}" class="btn btn-danger btn__delete">Delete</button>
         </td>
-      </tr>
-    `,
+      </tr>`,
     )
     .join("");
-
-  document.querySelectorAll(".btn__delete").forEach((btn) => {
-    btn.addEventListener("click", deleteProduct);
+  document.querySelectorAll(".btn__delete").forEach((btnDel) => {
+    btnDel.addEventListener("click", () => {
+      const id = btnDel.dataset.id;
+      confirm("Bạn có muốn xóa vĩnh viễn sản phẩm này không?") &&
+        deleteProduct(id);
+    });
   });
 };
 
-const deleteProduct = async (e) => {
-  e.preventDefault();
-  if (confirm("Bạn chắc chứ ?")) {
-    await fetch(`http://localhost:3000/products/${e.target.dataset.id}`, {
-      method: "DELETE",
-    });
-    await render();
-  }
-};
-
 document.addEventListener("DOMContentLoaded", render);
-
 const logout = () => {
   localStorage.removeItem("currentUser");
   window.location.href = "./login.html";
